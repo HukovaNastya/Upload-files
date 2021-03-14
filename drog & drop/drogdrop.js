9bf34a865b49d147bb65b48dea104dd782a082fb
HukovaNastya <62759919+HukovaNastya@users.noreply.github.com>
2021-03-14 12:25:00 +0200

    Update drogdrop.js

--- drog & drop/drogdrop.js
+++ drog & drop/drogdrop.js
@@ -1,6 +1,5 @@
 const dropSpace = document.querySelector('.drog-space');
 let file;
-let canDrop = true;
 dropSpace.addEventListener('dragover' ,(e) => {
   e.preventDefault();
   dropSpace.classList.add('active');
@@ -8,22 +7,16 @@ dropSpace.addEventListener('dragover' ,(e) => {
 });
 dropSpace.addEventListener('dragleave', (e) => {
   dropSpace.classList.remove('active');
-  canDrop = true;
 })
 dropSpace.addEventListener('drop', (e) => {
   e.preventDefault();
   file = e.dataTransfer.files[0];
   let fileType = file.type;
   let fileName = file.name;
-  canDrop = false;
- 
   if ( fileType === 'image/png' || fileType === 'image/jpg'  || fileType === 'image/jpeg' ){
     console.log(fileName);
   }else{
     alert(' You can drop only file with type: png,jpg,jpeg!');
   }
   dropSpace.classList.remove('active');
-  if(canDrop === false ){
-   dropSpace.removeEventListener();
- }
 })
