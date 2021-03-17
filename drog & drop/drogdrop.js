71f75e67f9bed8a705d208b13cb8160f00af71c8
HukovaNastya <62759919+HukovaNastya@users.noreply.github.com>
2021-03-17 22:42:00 +0200

    Update drogdrop.js

--- drog & drop/drogdrop.js
+++ drog & drop/drogdrop.js
@@ -3,6 +3,12 @@ const div = document.querySelector('.info-title');
 const imgNAme = div.querySelector('p');
 const divInfoAboutSize = document.querySelector('.info-text');
 const imgSize = divInfoAboutSize.querySelector('p');
+const divWithButton = document.querySelector('.progress__inner__btn');
+const btnSave = divWithButton.querySelector('button');
+let file;
+
+
+
 // console.log(imgNAme);
 const availableFileTypes = {
   'image/png': true,
@@ -32,7 +38,7 @@ dropSpace.addEventListener('drop', (e) => {
     toggleDropSpaceClass();
     return;
   }
-  const [file] = e.dataTransfer.files;
+  file = e.dataTransfer.files[0];
   const {type,name,size} = file;
   if (availableFileTypes[type]){
     imgNAme.classList.add('info-title');
@@ -45,4 +51,30 @@ dropSpace.addEventListener('drop', (e) => {
     alert(' You can drop only file with type: png,jpg,jpeg!');
   }
   toggleDropSpaceClass();
+});
+
+function sentServer(body, cb) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', ' http://localhost:2121/upload');
+  xhr.addEventListener('load', () => {
+    const response = JSON.parse(xhr.responseText);
+    cb(response);
+  });
+
+  xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
+
+  xhr.addEventListener('error', () => {
+    console.log('error');
+  });
+
+  xhr.send(JSON.stringify(body)); 
+}
+
+btnSave.addEventListener('click', e => {
+  const newPost = {
+    body: 'name',
+  };
+  sentServer(newPost, response => {
+   console.log(response);
+  });
 });
